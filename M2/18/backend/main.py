85947c8075505c5f7f40563feb5d1e1d7eba7974
Ruslan <sounditbox@gmail.com>
2025-02-07 22:03:14 +0400

    WebChatGPT | Added prometheus and grafana(uncofigured)

--- M2/18/backend/main.py
+++ M2/18/backend/main.py
@@ -2,18 +2,28 @@ import os
 
 from dotenv import load_dotenv
 from fastapi import FastAPI, WebSocket, HTTPException
+from loguru import logger
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
-
+from prometheus_fastapi_instrumentator import Instrumentator
 from gpt import ChatGptService
 
+from prometheus_client import Counter
+
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
+logger.add("./logs/app.log", format="{time} - {level} - {message}",
+           level='DEBUG')
 load_dotenv()
 TOKEN = os.getenv("OPENAI_API_KEY")
 gpt = ChatGptService(TOKEN)
 gpt.set_prompt('Ты - самый лучший и полезный ассистент в мире!')
 
-
+METRIC = Counter(
+    "http_requested_versions_changed",
+    "Number of times any version LLM was set.",
+    labelnames=("vers",)
+)
 origins = [
     '*'
 ]
@@ -32,7 +42,9 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_text()
+            logger.info(f'User sends: {data}')
             answer = await gpt.add_message(data)
+            logger.info(f'Answer: {answer}')
             await websocket.send_text(answer)
     except Exception as e:
         print(f"WebSocket error: {e}")
@@ -71,6 +83,15 @@ async def set_version(version_request: VersionRequest):
 
 @app.get("/versions")
 async def get_versions():
+    logger.info('Getting versions')
     return {
         'versions': ["gpt-3.5-turbo", "gpt-4o", "gpt-4-turbo", "GPT-4o mini"]
     }
+
+
+@app.get("/metrics")
+async def get_metrics():
+    logger.info('Getting metrics')
+    return {'metrics': 'metrics'}
+
+
